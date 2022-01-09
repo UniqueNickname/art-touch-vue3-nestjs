9bdca4c64ab0e08cc74bc627a4357f72d921740f
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 00:36:30 +0600

    chore(global): create commitlint config
    -

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  formatter: '@commitlint/format',
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'refactor',
+        'style',
+        'chore',
+        'revert',
+        'test',
+        'build',
+      ],
+    ],
+    'scope-enum': [2, 'always', ['backend', 'client', 'global']],
+    'subject-case': [0],
+  },
+}
