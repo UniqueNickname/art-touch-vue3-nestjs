1658372e6e9826b2d36246e8b118156a76a7d8c4
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 01:26:37 +0600

    feat(backend): add @nestjs/config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000
+
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  await app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`
+    Server: http://localhost:${PORT}
+    `)
+  })
 }
 bootstrap()
