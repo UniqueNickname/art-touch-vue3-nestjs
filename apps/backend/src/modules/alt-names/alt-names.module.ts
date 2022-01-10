f621d20d6962b35e964ac352fe3e28f5dc0cd3c7
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 23:17:07 +0600

    fix(backend): import university module to alt names module

--- apps/backend/src/modules/alt-names/alt-names.module.ts
+++ apps/backend/src/modules/alt-names/alt-names.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common'
 import { AltNamesService } from './alt-names.service'
 import { AltNamesController } from './alt-names.controller'
 import { SequelizeModule } from '@nestjs/sequelize'
-import { CityAltName } from 'src/models/alt-names.model'
+import { CityAltName, UniversityAltName } from 'src/models/alt-names.model'
 import { CitiesModule } from '../cities/cities.module'
+import { UniversitiesModule } from '../universities/universities.module'
 
 @Module({
-  imports: [SequelizeModule.forFeature([CityAltName]), CitiesModule],
+  imports: [
+    SequelizeModule.forFeature([CityAltName, UniversityAltName]),
+    CitiesModule,
+    UniversitiesModule,
+  ],
   providers: [AltNamesService],
   controllers: [AltNamesController],
   exports: [AltNamesService],
