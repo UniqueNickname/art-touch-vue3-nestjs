08f414d7c119f1c9edf99f198ab838d64d3f1bcd
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 19:03:48 +0600

    feat(backend): create alt-names module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { CitiesModule } from './modules/cities/cities.module'
 import { DatabaseModule } from './modules/database/database.module'
+import { AltNamesModule } from './modules/alt-names/alt-names.module'
 
 @Module({
-  imports: [DatabaseModule, CitiesModule, DatabaseModule],
+  imports: [DatabaseModule, CitiesModule, DatabaseModule, AltNamesModule],
   controllers: [],
   providers: [],
 })
