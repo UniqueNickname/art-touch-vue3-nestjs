4d98c001bf69a2088596d497aee2ea72f9b46bfd
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 11:36:08 +0600

    feat(backend): install swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000
@@ -11,10 +12,22 @@ async function bootstrap() {
     type: VersioningType.URI,
   })
 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Art touch')
+    .setDescription('Art touch core service swagger')
+    .setVersion('1.0.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  const docUrl = '/'
+  SwaggerModule.setup(docUrl, app, document)
+
   await app.listen(PORT, () => {
     // eslint-disable-next-line no-console
     console.log(`
     Server: http://localhost:${PORT}
+
+    Docs: http://localhost:${PORT}${docUrl}
     `)
   })
 }
