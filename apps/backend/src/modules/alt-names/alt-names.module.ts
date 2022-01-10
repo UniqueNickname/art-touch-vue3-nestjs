f4d022c811da4d28237566b1636264d38f3e48b8
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 21:49:12 +0600

    feat(backend): send "bad request" error creating alt name for not existing city

--- apps/backend/src/modules/alt-names/alt-names.module.ts
+++ apps/backend/src/modules/alt-names/alt-names.module.ts
@@ -3,10 +3,12 @@ import { AltNamesService } from './alt-names.service'
 import { AltNamesController } from './alt-names.controller'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { CityAltName } from 'src/models/alt-names.model'
+import { CitiesModule } from '../cities/cities.module'
 
 @Module({
-  imports: [SequelizeModule.forFeature([CityAltName])],
+  imports: [SequelizeModule.forFeature([CityAltName]), CitiesModule],
   providers: [AltNamesService],
   controllers: [AltNamesController],
+  exports: [AltNamesService],
 })
 export class AltNamesModule {}
--- apps/backend/src/modules/alt-names/alt-names.service.ts
+++ apps/backend/src/modules/alt-names/alt-names.service.ts
@@ -1,17 +1,29 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { CityAltName } from 'src/models/alt-names.model'
 import { CreateAltNameDto } from '@art-touch/common/dist/dto/create-alt-name.dto'
+import { CitiesService } from 'src/modules/cities/cities.service'
 
 @Injectable()
 export class AltNamesService {
   constructor(
     @InjectModel(CityAltName)
-    private CityAltNameRepository: typeof CityAltName,
+    private cityAltNameRepository: typeof CityAltName,
+    private citiesService: CitiesService,
   ) {}
 
   async createCityName(dto: CreateAltNameDto) {
-    const altName = await this.CityAltNameRepository.create(dto)
+    try {
+      await this.citiesService.getById(dto.entityId)
+    } catch (error) {
+      throw new HttpException(
+        `City with id '${dto.entityId}' does not exist`,
+        HttpStatus.BAD_REQUEST,
+      )
+    }
+
+    const altName = await this.cityAltNameRepository.create(dto)
+
     return altName
   }
 }
