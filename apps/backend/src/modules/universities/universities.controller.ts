d127350766975a0521bfdea69d0375062fac2ac4
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 22:51:00 +0600

    feat(backend): create universities controller

---
+++ apps/backend/src/modules/universities/universities.controller.ts
@@ -0,0 +1,50 @@
+import { CreateUniversityDto } from '@art-touch/common/dist/dto/create-university.dto'
+import { GetUniversityDto } from '@art-touch/common/dist/dto/get-university.dto'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Version,
+} from '@nestjs/common'
+import { ApiOperation, ApiResponse } from '@nestjs/swagger'
+import { UniversitiesService } from './universities.service'
+
+@Controller('universities')
+export class UniversitiesController {
+  constructor(private universitiesService: UniversitiesService) {}
+
+  @ApiOperation({ summary: 'Add a new university' })
+  @ApiResponse({ status: HttpStatus.CREATED, type: GetUniversityDto })
+  @Version('1')
+  @Post()
+  create(@Body() dto: CreateUniversityDto) {
+    return this.universitiesService.create(dto)
+  }
+
+  @ApiOperation({ summary: 'Get all universities' })
+  @ApiResponse({ status: HttpStatus.OK, type: [GetUniversityDto] })
+  @Version('1')
+  @Get()
+  getAll() {
+    return this.universitiesService.getAll()
+  }
+
+  @ApiOperation({ summary: 'Get universities in the city' })
+  @ApiResponse({ status: HttpStatus.OK, type: [GetUniversityDto] })
+  @Version('1')
+  @Get('/by_city/:id')
+  getByCity(@Param('id') value: string) {
+    return this.universitiesService.getByCity(Number(value))
+  }
+
+  @ApiOperation({ summary: 'Get a university by id' })
+  @ApiResponse({ status: HttpStatus.OK, type: [GetUniversityDto] })
+  @Version('1')
+  @Get('/:id')
+  getById(@Param('id') cityId: string) {
+    return this.universitiesService.getById(Number(cityId))
+  }
+}
--- apps/backend/src/modules/universities/universities.module.ts
+++ apps/backend/src/modules/universities/universities.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { University } from 'src/models/university.model'
 import { UniversitiesService } from './universities.service'
+import { UniversitiesController } from './universities.controller'
 
 @Module({
   imports: [SequelizeModule.forFeature([University])],
   providers: [UniversitiesService],
   exports: [UniversitiesService],
+  controllers: [UniversitiesController],
 })
 export class UniversitiesModule {}
