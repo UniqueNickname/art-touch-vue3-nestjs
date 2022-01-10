3067f63be234b64c9d5d94fd0c1b4efab5c760cf
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 23:49:50 +0600

    feat(backend): send "bad request" error when creating university of not existing city

--- apps/backend/src/modules/universities/universities.module.ts
+++ apps/backend/src/modules/universities/universities.module.ts
@@ -3,9 +3,10 @@ import { SequelizeModule } from '@nestjs/sequelize'
 import { University } from 'src/models/university.model'
 import { UniversitiesService } from './universities.service'
 import { UniversitiesController } from './universities.controller'
+import { CitiesModule } from '../cities/cities.module'
 
 @Module({
-  imports: [SequelizeModule.forFeature([University])],
+  imports: [SequelizeModule.forFeature([University]), CitiesModule],
   providers: [UniversitiesService],
   exports: [UniversitiesService],
   controllers: [UniversitiesController],
--- apps/backend/src/modules/universities/universities.service.ts
+++ apps/backend/src/modules/universities/universities.service.ts
@@ -3,14 +3,25 @@ import { InjectModel } from '@nestjs/sequelize'
 import { University } from 'src/models/university.model'
 import { CreateUniversityDto } from '@art-touch/common/dist/dto/create-university.dto'
 import { GetUniversityDto } from '@art-touch/common/dist/dto/get-university.dto'
+import { CitiesService } from '../cities/cities.service'
 
 @Injectable()
 export class UniversitiesService {
   constructor(
     @InjectModel(University) private universityRepository: typeof University,
+    private citiesService: CitiesService,
   ) {}
 
   async create(dto: CreateUniversityDto): Promise<GetUniversityDto> {
+    try {
+      await this.citiesService.getById(dto.cityId)
+    } catch (error) {
+      throw new HttpException(
+        `City with id '${dto.cityId}' does not exist`,
+        HttpStatus.BAD_REQUEST,
+      )
+    }
+
     try {
       const university = await this.universityRepository.create(dto)
       return {
