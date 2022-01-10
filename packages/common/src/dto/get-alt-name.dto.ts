b8fb50f8db38b0c3efb354ebaedd1e0da6d2beed
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 19:24:18 +0600

    fix(common): set GetAltNamesDTO fields as optional

--- packages/common/src/dto/get-alt-name.dto.ts
+++ packages/common/src/dto/get-alt-name.dto.ts
@@ -1,18 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { ISO } from '../enums/iso.enum'
 
-export class GetAltNamesDTO implements Record<ISO, string> {
+export class GetAltNamesDTO implements Partial<Record<ISO, string>> {
   @ApiProperty({
     example: 'Нью Йорк',
     description: 'Name of the city for Russian',
     uniqueItems: true,
   })
-  readonly 'ru-RU': string
+  'ru-RU'?: string
 
   @ApiProperty({
     example: 'New York',
     description: 'Name of the city for United States',
     uniqueItems: true,
   })
-  readonly 'en-US': string
+  'en-US'?: string
 }
