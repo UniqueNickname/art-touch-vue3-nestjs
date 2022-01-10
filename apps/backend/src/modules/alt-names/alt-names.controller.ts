0d0e80f3557752df1275e175f43076fa98f9e285
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 22:59:34 +0600

    feat(backend): create controller for creating alt names of universities

--- apps/backend/src/modules/alt-names/alt-names.controller.ts
+++ apps/backend/src/modules/alt-names/alt-names.controller.ts
@@ -1,7 +1,7 @@
 import { CreateAltNameDto } from '@art-touch/common/dist/dto/create-alt-name.dto'
 import { Body, Controller, HttpStatus, Post, Version } from '@nestjs/common'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
-import { CityAltName } from 'src/models/alt-names.model'
+import { CityAltName, UniversityAltName } from 'src/models/alt-names.model'
 import { AltNamesService } from './alt-names.service'
 
 @Controller()
@@ -15,5 +15,14 @@ export class AltNamesController {
   @Post('/cities/alt-names')
   createCityName(@Body() dto: CreateAltNameDto) {
     return this.altNamesService.createCityName(dto)
+  }
+
+  @ApiOperation({ summary: 'Add alternative name of the university' })
+  @ApiResponse({ status: HttpStatus.OK, type: UniversityAltName })
+  @ApiTags('Universities')
+  @Version('1')
+  @Post('/universities/alt-names')
+  createUniversityName(@Body() dto: CreateAltNameDto) {
+    return this.altNamesService.createUniversityName(dto)
   }
 }
