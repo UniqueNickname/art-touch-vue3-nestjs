1a71fe1be87b6e8077459a2e15c91d06eed09d9d
UniqueNickname <dr.art-47@yandex.ru>
2022-01-10 23:09:06 +0600

    feat(backend): add university alt names & universities models top database

--- apps/backend/src/modules/database/database.module.ts
+++ apps/backend/src/modules/database/database.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { SequelizeModule } from '@nestjs/sequelize'
-import { CityAltName } from 'src/models/alt-names.model'
+import { CityAltName, UniversityAltName } from 'src/models/alt-names.model'
 import { City } from 'src/models/city.model'
+import { University } from 'src/models/university.model'
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { City } from 'src/models/city.model'
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [City, CityAltName],
+      models: [City, CityAltName, University, UniversityAltName],
       autoLoadModels: true,
     }),
   ],
