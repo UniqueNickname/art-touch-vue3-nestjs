22d23383696a7b95d21855d05f0aaace8c97a296
UniqueNickname <dr.art-47@yandex.ru>
2022-01-11 15:16:18 +0600

    feat(backend): create teachers service

--- apps/backend/src/modules/teachers/teachers.module.ts
+++ apps/backend/src/modules/teachers/teachers.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { Teacher } from 'src/models/teacher.model'
+import { TeachersService } from './teachers.service'
+import { UniversitiesModule } from '../universities/universities.module'
 
-@Module({})
+@Module({
+  imports: [SequelizeModule.forFeature([Teacher]), UniversitiesModule],
+  providers: [TeachersService],
+  exports: [TeachersService],
+})
 export class TeachersModule {}
---
+++ apps/backend/src/modules/teachers/teachers.service.ts
@@ -0,0 +1,97 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/sequelize'
+import { Teacher } from 'src/models/teacher.model'
+import { CreateTeacherDto } from '@art-touch/common/dist/dto/create-teacher.dto'
+import { GetTeacherDto } from '@art-touch/common/dist/dto/get-teacher.dto'
+import { UniversitiesService } from '../universities/universities.service'
+import { TeacherAltName } from 'src/models/alt-names.model'
+import { GetAltNamesDTO } from '@art-touch/common/dist/dto/get-alt-name.dto'
+
+@Injectable()
+export class TeachersService {
+  constructor(
+    @InjectModel(Teacher) private teacherRepository: typeof Teacher,
+    private universitiesService: UniversitiesService,
+  ) {}
+
+  async create(dto: CreateTeacherDto): Promise<GetTeacherDto> {
+    try {
+      await this.universitiesService.getById(dto.universityId)
+    } catch (error) {
+      throw new HttpException(
+        `University with id '${dto.universityId}' does not exist`,
+        HttpStatus.BAD_REQUEST,
+      )
+    }
+
+    const teacher = await this.teacherRepository.create(dto)
+    return {
+      id: teacher.id,
+      universityId: teacher.universityId,
+      name: teacher.name,
+      altNames: {},
+    }
+  }
+
+  async getAll(): Promise<GetTeacherDto[]> {
+    const teachers = await this.teacherRepository.findAll({
+      include: [TeacherAltName],
+    })
+
+    return teachers.map(teacher => ({
+      id: teacher.id,
+      universityId: teacher.universityId,
+      name: teacher.name,
+      altNames: this.transformAltNames(teacher.altNames),
+    }))
+  }
+
+  async getById(teacherId: number): Promise<GetTeacherDto> {
+    const teacher = await this.teacherRepository.findOne({
+      where: { id: teacherId },
+      include: [TeacherAltName],
+    })
+
+    if (!teacher) {
+      throw new HttpException(
+        `Teacher with Id ${teacherId} does not exist.`,
+        HttpStatus.NOT_FOUND,
+      )
+    }
+
+    return {
+      id: teacher.id,
+      universityId: teacher.universityId,
+      name: teacher.name,
+      altNames: this.transformAltNames(teacher.altNames),
+    }
+  }
+
+  async getByUniversity(universityId: number): Promise<GetTeacherDto[]> {
+    const teachers = await this.teacherRepository.findAll({
+      where: { universityId },
+      include: [TeacherAltName],
+    })
+
+    if (!teachers.length) {
+      throw new HttpException(
+        `Teachers for university with Id ${universityId} does not exist.`,
+        HttpStatus.NOT_FOUND,
+      )
+    }
+
+    return teachers.map(teacher => ({
+      id: teacher.id,
+      universityId: teacher.universityId,
+      name: teacher.name,
+      altNames: this.transformAltNames(teacher.altNames),
+    }))
+  }
+
+  private transformAltNames(altNames: TeacherAltName[]) {
+    return altNames.reduce((acc, { value, iso }) => {
+      acc[iso] = value
+      return acc
+    }, {} as GetAltNamesDTO)
+  }
+}
