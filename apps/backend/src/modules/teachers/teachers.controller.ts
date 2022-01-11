0ac07bfb4518aadeb45848d5727c13a1622d4ff9
UniqueNickname <dr.art-47@yandex.ru>
2022-01-11 15:16:57 +0600

    feat(backend): create teachers controller

---
+++ apps/backend/src/modules/teachers/teachers.controller.ts
@@ -0,0 +1,51 @@
+import { CreateTeacherDto } from '@art-touch/common/dist/dto/create-teacher.dto'
+import { GetTeacherDto } from '@art-touch/common/dist/dto/get-teacher.dto'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Version,
+} from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { TeachersService } from './teachers.service'
+
+@ApiTags('Teachers')
+@Controller('teachers')
+export class TeachersController {
+  constructor(private teachersService: TeachersService) {}
+
+  @ApiOperation({ summary: 'Add a new teacher' })
+  @ApiResponse({ status: HttpStatus.CREATED, type: GetTeacherDto })
+  @Version('1')
+  @Post()
+  create(@Body() dto: CreateTeacherDto) {
+    return this.teachersService.create(dto)
+  }
+
+  @ApiOperation({ summary: 'Get all teachers' })
+  @ApiResponse({ status: HttpStatus.CREATED, type: GetTeacherDto })
+  @Version('1')
+  @Get()
+  getAll() {
+    return this.teachersService.getAll()
+  }
+
+  @ApiOperation({ summary: 'Get a teacher by id' })
+  @ApiResponse({ status: HttpStatus.OK, type: [GetTeacherDto] })
+  @Version('1')
+  @Get('/:id')
+  getById(@Param('id') cityId: string) {
+    return this.teachersService.getById(Number(cityId))
+  }
+
+  @ApiOperation({ summary: 'Get teachers in a university' })
+  @ApiResponse({ status: HttpStatus.OK, type: [GetTeacherDto] })
+  @Version('1')
+  @Get('/by-university/:id')
+  getByCity(@Param('id') value: string) {
+    return this.teachersService.getByUniversity(Number(value))
+  }
+}
--- apps/backend/src/modules/teachers/teachers.module.ts
+++ apps/backend/src/modules/teachers/teachers.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { Teacher } from 'src/models/teacher.model'
 import { TeachersService } from './teachers.service'
+import { TeachersController } from './teachers.controller'
 import { UniversitiesModule } from '../universities/universities.module'
 
 @Module({
   imports: [SequelizeModule.forFeature([Teacher]), UniversitiesModule],
   providers: [TeachersService],
   exports: [TeachersService],
+  controllers: [TeachersController],
 })
 export class TeachersModule {}
