099844ff6368c10c553a46ef30976e7e4bed01c2
UniqueNickname <dr.art-47@yandex.ru>
2022-01-19 12:41:24 +0600

    feat(backend): add @nestjs/serve-static

--- apps/backend/src/modules/files/files.module.ts
+++ apps/backend/src/modules/files/files.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import * as path from 'path'
 import { FilesService } from './files.service'
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, '../../static'),
+    }),
+  ],
   providers: [FilesService],
   exports: [FilesService],
 })
