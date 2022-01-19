dba36f912cbcb6cbb19bf757b0015250a78838c0
UniqueNickname <dr.art-47@yandex.ru>
2022-01-19 12:36:29 +0600

    feat(backend): create files service

--- apps/backend/src/modules/files/files.module.ts
+++ apps/backend/src/modules/files/files.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common'
+import { FilesService } from './files.service'
 
-@Module({})
+@Module({
+  providers: [FilesService],
+  exports: [FilesService],
+})
 export class FilesModule {}
