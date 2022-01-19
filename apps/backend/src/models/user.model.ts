8cf236d720d4087f856936bf32064d01401fe077
UniqueNickname <dr.art-47@yandex.ru>
2022-01-19 14:57:11 +0600

    feat(backend): create admin model

--- apps/backend/src/models/user.model.ts
+++ apps/backend/src/models/user.model.ts
@@ -27,6 +27,8 @@ interface JuryCreationAttrs extends UserCreationAttrs {
   photo: string
 }
 
+type AdminAttrs = UserCreationAttrs
+
 @Table({ tableName: 'participants' })
 export class Participant extends Model<Participant, ParticipantCreationAttrs> {
   @Column({
@@ -81,4 +83,27 @@ export class Jury extends Model<Jury, JuryCreationAttrs> {
 
   @Column({ type: DataType.STRING, allowNull: false })
   photo: string
+}
+
+@Table({ tableName: 'admins' })
+export class Admin extends Model<Admin, AdminAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  password: string
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  phone: string
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  fullName: string
 }
