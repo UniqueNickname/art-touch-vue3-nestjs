1e198af8dc068940f7c7d3da704b5392c36d58f6
UniqueNickname <dr.art-47@yandex.ru>
2022-01-21 12:34:44 +0600

    feat(backend): ability to upload files of any extensions

--- apps/backend/src/modules/files/files.service.ts
+++ apps/backend/src/modules/files/files.service.ts
@@ -5,18 +5,21 @@ import * as uuid from 'uuid'
 
 @Injectable()
 export class FilesService {
-  async createFile(file): Promise<string> {
+  async createFile(file: Express.Multer.File): Promise<string> {
     try {
-      const fileName = uuid.v4() + '.jpg'
+      const fileExtension = file.originalname.split('.').pop()
+      const fileName = `${uuid.v4()}.${fileExtension}`
       const filePath = path.resolve(__dirname, '../../static')
+
       if (!fs.existsSync(filePath)) {
         fs.mkdirSync(filePath, { recursive: true })
       }
+
       fs.writeFileSync(path.join(filePath, fileName), file.buffer)
       return fileName
     } catch (e) {
       throw new HttpException(
-        'Произошла ошибка при записи файла',
+        'An error occurred while writing the file',
         HttpStatus.INTERNAL_SERVER_ERROR,
       )
     }
