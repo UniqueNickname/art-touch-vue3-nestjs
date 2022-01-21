05c350e1fdcfcb27b9e20d6d3627792bc30f9894
UniqueNickname <dr.art-47@yandex.ru>
2022-01-21 15:46:02 +0600

    feat(backend): create admin module

--- apps/backend/src/modules/auth/auth.module.ts
+++ apps/backend/src/modules/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { JuryModule } from './jury/jury.module'
 import { ParticipantsModule } from './participants/participants.module'
+import { AdminModule } from './admin/admin.module'
 
 @Module({
-  imports: [JuryModule, ParticipantsModule],
+  imports: [JuryModule, ParticipantsModule, AdminModule],
 })
 export class AuthModule {}
