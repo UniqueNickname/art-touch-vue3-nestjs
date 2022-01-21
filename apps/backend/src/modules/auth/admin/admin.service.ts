1d3245ac9fd1c00bf6e7db52aa3d09ef42540912
UniqueNickname <dr.art-47@yandex.ru>
2022-01-21 21:46:55 +0600

    feat(backend): create method for getting admin by email

--- apps/backend/src/modules/auth/admin/admin.service.ts
+++ apps/backend/src/modules/auth/admin/admin.service.ts
@@ -1,5 +1,5 @@
 import { CreateAdminDto } from '@art-touch/common/dist/dto/create-admin.dto'
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { Admin } from 'src/models/user.model'
 
@@ -9,6 +9,21 @@ export class AdminService {
 
   async create(dto: CreateAdminDto): Promise<Admin> {
     const admin = await this.adminRepository.create(dto)
+
+    return admin
+  }
+
+  async getByEmail(email: string): Promise<Admin> {
+    const admin = await this.adminRepository.findOne({
+      where: { email },
+    })
+
+    if (!admin) {
+      throw new HttpException(
+        `Admin with email "${email}" does not exist.`,
+        HttpStatus.NOT_FOUND,
+      )
+    }
 
     return admin
   }
