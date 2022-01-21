80ae435f63024d1905e504faec559ec0dcdc397f
UniqueNickname <dr.art-47@yandex.ru>
2022-01-21 21:45:15 +0600

    feat(backend): create method for getting participant by email

--- apps/backend/src/modules/auth/participants/participants.service.ts
+++ apps/backend/src/modules/auth/participants/participants.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { Participant } from 'src/models/user.model'
 import { CreateParticipantDto } from '@art-touch/common/dist/dto/create-participant.dto'
@@ -11,6 +11,28 @@ export class ParticipantsService {
 
   async create(dto: CreateParticipantDto): Promise<Participant> {
     const participant = await this.participantRepository.create(dto)
+
+    return participant
+  }
+
+  async getByEmail(
+    email: string,
+    type: 'model' | 'dto' = 'dto',
+  ): Promise<Participant | Participant> {
+    const participant = await this.participantRepository.findOne({
+      where: { email },
+    })
+
+    if (!participant) {
+      throw new HttpException(
+        `Participant with email "${email}" does not exist.`,
+        HttpStatus.NOT_FOUND,
+      )
+    }
+
+    if (type === 'model') {
+      return participant
+    }
 
     return participant
   }
