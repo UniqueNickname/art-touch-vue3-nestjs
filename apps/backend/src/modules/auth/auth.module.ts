2588d366e9b6d19374e37edb669305607234ec59
UniqueNickname <dr.art-47@yandex.ru>
2022-01-21 17:01:00 +0600

    feat(backend): add jwt module

--- apps/backend/src/modules/auth/auth.module.ts
+++ apps/backend/src/modules/auth/auth.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common'
 import { JuryModule } from './jury/jury.module'
 import { ParticipantsModule } from './participants/participants.module'
 import { AdminModule } from './admin/admin.module'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [JuryModule, ParticipantsModule, AdminModule],
+  imports: [
+    JwtModule.register({
+      secret: process.env.ACCESS_KEY || '_34fhR/fhR3-',
+      signOptions: {
+        expiresIn: '24h',
+      },
+    }),
+    JuryModule,
+    ParticipantsModule,
+    AdminModule,
+  ],
 })
 export class AuthModule {}
