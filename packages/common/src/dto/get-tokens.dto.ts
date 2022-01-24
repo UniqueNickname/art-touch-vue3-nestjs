77dff243c88a49c409be1d15a714e27851830384
UniqueNickname <dr.art-47@yandex.ru>
2022-01-24 14:06:49 +0600

    feat(common): create tokens dto

---
+++ packages/common/src/dto/get-tokens.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { Role } from '../enums/role.enum'
+
+export class TokenPayload {
+  readonly id: string
+
+  readonly fullName: string
+
+  readonly role: Role
+}
+
+export class Tokens {
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    description: 'Access token',
+    uniqueItems: true,
+  })
+  readonly access: string
+
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    description: 'Id of a city',
+    uniqueItems: true,
+  })
+  readonly refresh: string
+}
