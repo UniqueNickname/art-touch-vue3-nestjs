c3a630e100b24d0ba85c0c45b3289c5d17780b5d
UniqueNickname <dr.art-47@yandex.ru>
2022-01-26 21:18:24 +0600

    feat(client): set global theme for primary buttons

--- apps/client/src/App.vue
+++ apps/client/src/App.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
+  <n-config-provider :theme-overrides="themeOverrides">
     <router-view />
-  </div>
+  </n-config-provider>
 </template>
+
+<script setup lang="ts">
+import { NConfigProvider, GlobalThemeOverrides } from 'naive-ui'
+import colors from 'tailwindcss/colors'
+
+const themeOverrides: GlobalThemeOverrides = {
+  Button: {
+    // base
+    colorPrimary: colors.purple[800],
+    borderPrimary: colors.purple[800],
+    textColorPrimary: colors.white,
+    // hover
+    colorHoverPrimary: colors.purple[700],
+    borderHoverPrimary: colors.purple[700],
+    textColorHoverPrimary: colors.white,
+    //pressed
+    colorPressedPrimary: colors.purple[500],
+    borderPressedPrimary: colors.purple[500],
+    textColorPressedPrimary: colors.white,
+    // focus
+    colorFocusPrimary: colors.purple[700],
+    borderFocusPrimary: colors.purple[700],
+    textColorFocusPrimary: colors.white,
+  },
+}
+</script>
