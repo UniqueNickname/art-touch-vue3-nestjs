8473e4501c28d3fbaaac4fdc0af38bf9ab7e58a5
UniqueNickname <dr.art-47@yandex.ru>
2022-01-26 22:40:21 +0600

    build(client): add xs breakpoint to tailwind

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,7 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      ...defaultTheme.screens,
+      xs: '475px',
+    },
   },
   plugins: [],
 }
