0283cb17c31d198c2ee5c863f853860f73d32aa3
UniqueNickname <dr.art-47@yandex.ru>
2022-01-26 16:33:12 +0600

    feat(client): install @vueuse/head

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,15 +1,18 @@
 import { createApp } from 'vue'
 import { createRouter } from './router'
-import App from './App.vue'
 import { createI18N } from './locale/create-i18n'
+import { createHead } from '@vueuse/head'
+import App from './App.vue'
 
 const app = createApp(App)
 
 const router = createRouter()
 const i18n = createI18N()
+const head = createHead()
 
 app.use(router)
 app.use(i18n)
+app.use(head)
 
 router.isReady().then(() => {
   app.mount('#app', true)
