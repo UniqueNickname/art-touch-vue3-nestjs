4a5644ae9c0768b778f66b3b8f19df861d428c6d
UniqueNickname <dr.art-47@yandex.ru>
2022-01-26 18:00:48 +0600

    build(client): add alias to src to vite config

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pages from 'vite-plugin-pages'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), pages()],
+  resolve: {
+    alias: {
+      src: path.join(__dirname, 'src'),
+    },
+  },
 })
