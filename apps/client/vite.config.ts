92cef02c35c80ac4dad9f9199e2f169b9f6256b0
UniqueNickname <dr.art-47@yandex.ru>
2022-01-26 18:46:30 +0600

    build(client): connect dotenv to vite

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pages from 'vite-plugin-pages'
+import dotenv from 'dotenv'
 import * as path from 'path'
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
 
 // https://vitejs.dev/config/
 export default defineConfig({
