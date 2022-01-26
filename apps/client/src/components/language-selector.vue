c21ba64553c6a7654deef0743ef643de22c2c4f3
UniqueNickname <dr.art-47@yandex.ru>
2022-01-26 21:22:13 +0600

    feat(client): create language selector

---
+++ apps/client/src/components/language-selector.vue
@@ -0,0 +1,46 @@
+<template>
+  <n-dropdown
+    v-if="!isSSR()"
+    trigger="hover"
+    :options="options"
+    @select="changeLanguage"
+  >
+    <n-button text>{{ locale.split('-')[1] }}</n-button>
+  </n-dropdown>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useCookies } from 'vue3-cookies'
+import { NDropdown, NButton } from 'naive-ui/lib'
+import { isSSR } from 'src/utils/isSSR'
+
+const { locale, availableLocales } = useI18n()
+const { cookies } = useCookies()
+
+const COOKIES_LANG_KEY = 'i18n_redirected'
+
+if (!isSSR()) {
+  const cookiesLanguage = cookies.get(COOKIES_LANG_KEY)
+  if (cookiesLanguage) {
+    locale.value = cookiesLanguage
+  }
+}
+
+const options = computed(() => {
+  return availableLocales.map(currentLocale => ({
+    label: currentLocale.split('-')[1],
+    key: currentLocale,
+  }))
+})
+
+const changeLanguage = (event: string) => {
+  locale.value = event
+  if (isSSR()) {
+    return
+  }
+  cookies.set(COOKIES_LANG_KEY, locale.value)
+  document.documentElement.setAttribute('lang', locale.value.toLowerCase())
+}
+</script>
