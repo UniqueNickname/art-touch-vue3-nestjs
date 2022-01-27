2793d68dedb50e2ecafd78b1be365e441088b123
UniqueNickname <dr.art-47@yandex.ru>
2022-01-27 19:25:11 +0600

    feat(client): create auth layout

---
+++ apps/client/src/layouts/auth-layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="min-h-screen sm:bg-gradient-to-t from-red-300 to-violet-500 bg-none"
+  >
+    <div class="min-h-screen">
+      <app-navigation />
+      <div class="sm:pt-24 sm:pb-6 pb-4 pt-4">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppNavigation from 'src/components/app-navigation.vue'
+</script>
