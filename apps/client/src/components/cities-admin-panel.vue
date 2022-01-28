d445b0aab3a2e56be394fae6da86b7d3a827017d
UniqueNickname <dr.art-47@yandex.ru>
2022-01-28 18:04:27 +0600

    feat(client): create panel for modifying cities

---
+++ apps/client/src/components/cities-admin-panel.vue
@@ -0,0 +1,22 @@
+<template>
+  <n-collapse>
+    <n-collapse-item :title="t('admin.tabs.cities.add')" name="cities">
+      <add-city />
+    </n-collapse-item>
+    <n-collapse-item
+      :title="t('admin.tabs.cities.add-altname')"
+      name="cities-altnames"
+    >
+      <add-city-altname />
+    </n-collapse-item>
+  </n-collapse>
+</template>
+
+<script setup lang="ts">
+import { NCollapse, NCollapseItem } from 'naive-ui'
+import { useI18n } from 'vue-i18n'
+import AddCity from 'src/components/add-city.vue'
+import AddCityAltname from 'src/components/add-city-altname.vue'
+
+const { t } = useI18n()
+</script>
