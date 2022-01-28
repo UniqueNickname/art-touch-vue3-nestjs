0bf686a5f7f6fe2f7f1542fac87e6211184e029b
UniqueNickname <dr.art-47@yandex.ru>
2022-01-28 18:16:00 +0600

    feat(client): create container layout

---
+++ apps/client/src/layouts/container-layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <default-layout>
+    <div
+      class="mx-auto max-w-5xl w-full bg-white my-6 pt-4 pb-8 px-10 rounded-xl"
+    >
+      <h1 class="text-center font-bold text-2xl mb-4">
+        {{ title }}
+      </h1>
+      <slot />
+    </div>
+  </default-layout>
+</template>
+
+<script setup lang="ts">
+import DefaultLayout from 'src/layouts/default-layout.vue'
+
+interface Props {
+  title: string
+}
+
+defineProps<Props>()
+</script>
