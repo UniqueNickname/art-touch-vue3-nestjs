931d1189b98d48632c4798fddb99be7f46429a67
UniqueNickname <dr.art-47@yandex.ru>
2022-01-31 22:19:40 +0600

    feat(client): add TokenPayload & Tokens type instead common types

--- apps/client/src/types/index.ts
+++ apps/client/src/types/index.ts
@@ -1,7 +1,15 @@
-export type Role =
-  | 'admin'
-  | 'jury'
-  | 'participant'
-  | 'authorized'
-  | 'unauthorized'
-  | 'all'
+export interface TokenPayload {
+  readonly id: string
+
+  readonly fullName: string
+
+  readonly role: 'admin' | 'jury' | 'participant'
+}
+
+export interface Tokens {
+  access: string
+
+  refresh: string
+}
+
+export type Role = TokenPayload['role'] | 'authorized' | 'unauthorized' | 'all'
