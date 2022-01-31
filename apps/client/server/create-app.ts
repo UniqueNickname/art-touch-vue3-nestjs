c1c00f5c1c41745d656c729331ced835b24297e6
UniqueNickname <dr.art-47@yandex.ru>
2022-01-31 17:10:39 +0600

    feat(client): install pinia

--- apps/client/server/create-app.ts
+++ apps/client/server/create-app.ts
@@ -1,6 +1,7 @@
 import { ISO } from '../../../packages/common/src/enums/iso.enum'
 import { createSSRApp, createApp as createVueApp } from 'vue'
 import { createHead } from '@vueuse/head'
+import { createPinia } from 'pinia'
 import { createRouter } from '../src/router'
 import { createI18N } from '../src/locale/create-i18n'
 import { isSSR } from '../src/utils/isSSR'
@@ -13,10 +14,12 @@ export async function createApp(language: ISO = ISO.en) {
   const head = createHead()
   const router = createRouter()
   const i18n = createI18N(language)
+  const pinia = createPinia()
 
   app.use(head)
   app.use(router)
   app.use(i18n)
+  app.use(pinia)
 
   return { app, head, router }
 }
