2646e796e5da8184de617e7696241f0247f922e7
UniqueNickname <dr.art-47@yandex.ru>
2022-02-02 00:41:01 +0600

    fix(client): add margin to container layout

--- apps/client/src/layouts/container-layout.vue
+++ apps/client/src/layouts/container-layout.vue
@@ -1,12 +1,14 @@
 <template>
   <default-layout :role="role">
-    <div
-      class="w-full max-w-5xl px-10 pt-4 pb-8 mx-auto my-6 bg-white rounded-xl"
-    >
-      <h1 class="mb-4 text-2xl font-bold text-center">
-        {{ title }}
-      </h1>
-      <slot />
+    <div class="w-full max-w-5xl mx-auto sm:px-10">
+      <div
+        class="w-full max-w-5xl px-10 pt-4 pb-8 mx-auto my-6 bg-white rounded-xl"
+      >
+        <h1 class="mb-4 text-2xl font-bold text-center">
+          {{ title }}
+        </h1>
+        <slot />
+      </div>
     </div>
   </default-layout>
 </template>
