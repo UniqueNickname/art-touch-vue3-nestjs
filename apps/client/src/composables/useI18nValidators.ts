c78fd06ecddd6a8c9f94a54e7da5a85ab881bc0e
UniqueNickname <dr.art-47@yandex.ru>
2022-02-02 02:37:16 +0600

    fix(client): minLength validator message

--- apps/client/src/composables/useI18nValidators.ts
+++ apps/client/src/composables/useI18nValidators.ts
@@ -1,9 +1,25 @@
 import { useI18n } from 'vue-i18n'
 import * as validators from '@vuelidate/validators'
 
+interface MinLengthParams {
+  min: number
+  type: 'minLength'
+}
+
 export const useI18nValidators = () => {
   const { t } = useI18n()
-  const withI18nMessage = validators.createI18nMessage({ t })
+  const withI18nMessage = validators.createI18nMessage({
+    t,
+    messagePath: params => {
+      if (params.$validator === 'minLength') {
+        if ((params.$params as MinLengthParams).min > 4) {
+          return `validations.${params.$validator}Big`
+        }
+        return `validations.${params.$validator}Small`
+      }
+      return `validations.${params.$validator}`
+    },
+  })
 
   const required = withI18nMessage(validators.required)
   const email = withI18nMessage(validators.email)
