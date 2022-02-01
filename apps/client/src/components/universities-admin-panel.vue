ec761248c004a9b3006d43cfb842c1b03a69d5ad
UniqueNickname <dr.art-47@yandex.ru>
2022-02-01 20:51:03 +0600

    feat(client): create panel for modifying universities

---
+++ apps/client/src/components/universities-admin-panel.vue
@@ -0,0 +1,22 @@
+<template>
+  <n-collapse>
+    <n-collapse-item :title="t('admin.tabs.universities.add')" name="cities">
+      <add-university />
+    </n-collapse-item>
+    <n-collapse-item
+      :title="t('admin.tabs.cities.add-altname')"
+      name="cities-altnames"
+    >
+      <add-university-altname />
+    </n-collapse-item>
+  </n-collapse>
+</template>
+
+<script setup lang="ts">
+import { NCollapse, NCollapseItem } from 'naive-ui'
+import { useI18n } from 'vue-i18n'
+import AddUniversity from 'src/components/add-university.vue'
+import AddUniversityAltname from 'src/components/add-university-altname.vue'
+
+const { t } = useI18n()
+</script>
