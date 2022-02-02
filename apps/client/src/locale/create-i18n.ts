3c3f430edff3d5735ac197bf616a6ec71b620fe9
UniqueNickname <dr.art-47@yandex.ru>
2022-02-02 15:33:50 +0600

    refactor(client): add exported ISO type

--- apps/client/src/locale/create-i18n.ts
+++ apps/client/src/locale/create-i18n.ts
@@ -1,9 +1,9 @@
+import type { ISO } from 'src/types'
 import { createI18n } from 'vue-i18n'
-import { ISO } from '../../../../packages/common/src/enums/iso.enum'
 import ru from './ru-RU.json'
 import en from './en-US.json'
 
-export function createI18N(defaultLanguage: ISO = ISO.en) {
+export function createI18N(defaultLanguage: ISO = 'en-US') {
   const messages = { 'ru-RU': ru, 'en-US': en }
 
   return createI18n({
---
+++ apps/client/src/types/index.ts
@@ -0,0 +1,1 @@
+export type ISO = 'en-US' | 'ru-RU'
