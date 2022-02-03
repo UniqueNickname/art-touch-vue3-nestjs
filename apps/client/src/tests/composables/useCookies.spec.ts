90d061a2c405d242400fce8e029c22fa26cd8452
UniqueNickname <dr.art-47@yandex.ru>
2022-02-04 00:00:15 +0600

    test(client): set method of useCookies

--- apps/client/src/tests/composables/useCookies.spec.ts
+++ apps/client/src/tests/composables/useCookies.spec.ts
@@ -2,10 +2,16 @@ import { useCookies } from '../../composables/useCookies'
 
 describe('useCookies', () => {
   const cookieName = 'token'
+  const cookieValue = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
 
-  const { get } = useCookies()
+  const { get, set } = useCookies()
 
   it("Method get should return null when cookie doesn't exist", async () => {
     expect(get(cookieName)).toBe(null)
+  })
+
+  it('After setting cookie method get should return specified value', async () => {
+    set(cookieName, cookieValue)
+    expect(get(cookieName)).toBe(cookieValue)
   })
 })
