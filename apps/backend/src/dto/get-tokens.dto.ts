435647e145a1ab158636bebb293ede6562378583
UniqueNickname <dr.art-47@yandex.ru>
2022-02-04 16:23:03 +0600

    feat(backend): add description of TokenPayload to swagger

--- apps/backend/src/dto/get-tokens.dto.ts
+++ apps/backend/src/dto/get-tokens.dto.ts
@@ -2,10 +2,23 @@ import type { Role } from 'src/types'
 import { ApiProperty } from '@nestjs/swagger'
 
 export class TokenPayload {
+  @ApiProperty({
+    example: 1,
+    description: 'Id of the user',
+    uniqueItems: true,
+  })
   readonly id: string
 
+  @ApiProperty({
+    example: 'Paul Kreitman',
+    description: 'Full name of the user',
+  })
   readonly fullName: string
 
+  @ApiProperty({
+    example: 'Jury',
+    description: 'Role of the user',
+  })
   readonly role: Role
 }
 
