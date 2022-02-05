4c5f718e83e727b7580b7d5d36f434e5a474d98d
UniqueNickname <dr.art-47@yandex.ru>
2022-02-05 22:40:28 +0600

    test(client): getValidISO

--- apps/client/src/tests/runtime-type-checkers/iso.spec.ts
+++ apps/client/src/tests/runtime-type-checkers/iso.spec.ts
@@ -1,4 +1,4 @@
-import { checkISOType } from 'src/runtime-type-checkers/iso'
+import { checkISOType, getValidISO } from 'src/runtime-type-checkers/iso'
 
 describe('checkISOType', () => {
   it('Should return true if a value type is valid', async () => {
@@ -12,5 +12,21 @@ describe('checkISOType', () => {
     expect(checkISOType(1)).toBe(false)
     expect(checkISOType({})).toBe(false)
     expect(checkISOType(null)).toBe(false)
+  })
+})
+
+describe('getValidISO', () => {
+  it('Should return received value if a value type is valid', async () => {
+    expect(getValidISO('ru-RU')).toBe('ru-RU')
+    expect(getValidISO('en-US')).toBe('en-US')
+  })
+
+  it('Should return default value if a value type is not valid', async () => {
+    expect(getValidISO('en-us')).toBe('en-US')
+    expect(getValidISO('en-us', 'ru-RU')).toBe('ru-RU')
+    expect(getValidISO('string', 'ru-RU')).toBe('ru-RU')
+    expect(getValidISO(1, 'ru-RU')).toBe('ru-RU')
+    expect(getValidISO({}, 'ru-RU')).toBe('ru-RU')
+    expect(getValidISO(null, 'ru-RU')).toBe('ru-RU')
   })
 })
